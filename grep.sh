ab2d1aeb3e2b535c0bdf4c940ec0a24af714ef77
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-14 06:57:12 +0800

    Update grep.sh

--- grep.sh
+++ grep.sh
@@ -50,5 +50,6 @@ rg -oPHn "user ..............................................................." 
 rg -oPHn "user:..............................................................." *.js >> $output/4_grep.txt
 rg -oPHn "user=..............................................................." *.js >> $output/4_grep.txt
 rm -rf $output/3_js
+cd /root/script/3_httprobe
 sed -e '/+/d' $output/4_grep.txt | sed -e '/?/d' | sed -e '/!/d' | sed -e '/&/d' > 1.txt ; mv 1.txt $output/4_grep.txt
 touch $output/4_gp.txt ; sort -u $output/4_grep.txt -o $output/4_gp.txt ; rm $output/4_grep.txt 
