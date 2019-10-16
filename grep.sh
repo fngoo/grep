d979d4a8c61da24f00a4060f87d16ad51b9355f0
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-17 06:31:08 +0800

    Update grep.sh

--- grep.sh
+++ grep.sh
@@ -7,36 +7,14 @@ rg  -oPHn  "pwd=........................." *.js >>   $output/4_grep.txt
 rg  -oPHn  "pass ........................" *.js >>   $output/4_grep.txt
 rg  -oPHn  "pass:........................." *.js >>   $output/4_grep.txt
 rg  -oPHn  "pass=........................." *.js >>   $output/4_grep.txt
-rg -oPHn "addr ........................." *.js >> $output/4_grep.txt
-rg -oPHn "addr:........................." *.js >> $output/4_grep.txt
-rg -oPHn "addr=........................." *.js >> $output/4_grep.txt
 rg -oPHn "admin ........................." *.js >> $output/4_grep.txt
 rg -oPHn "admin:........................." *.js >> $output/4_grep.txt
 rg -oPHn "admin=........................." *.js >> $output/4_grep.txt
 rg -oPHn "manager ........................." *.js >> $output/4_grep.txt
 rg -oPHn "manager:........................." *.js >> $output/4_grep.txt
 rg -oPHn "manager=........................." *.js >> $output/4_grep.txt
-rg -oPHn "phone ........................." *.js >> $output/4_grep.txt
-rg -oPHn "phone:........................." *.js >> $output/4_grep.txt
-rg -oPHn "phone=........................." *.js >> $output/4_grep.txt
-rg -oPHn "priva ........................." *.js >> $output/4_grep.txt
-rg -oPHn "priva:........................." *.js >> $output/4_grep.txt
-rg -oPHn "priva=........................." *.js >> $output/4_grep.txt
-rg -oPHn "root ........................." *.js >> $output/4_grep.txt
-rg -oPHn "root:........................." *.js >> $output/4_grep.txt
-rg -oPHn "root=........................." *.js >> $output/4_grep.txt
-rg -oPHn "secret ........................." *.js >> $output/4_grep.txt
-rg -oPHn "secret:........................." *.js >> $output/4_grep.txt
-rg -oPHn "secret=........................." *.js >> $output/4_grep.txt
-rg -oPHn "smpt ........................." *.js >> $output/4_grep.txt
-rg -oPHn "smpt:........................." *.js >> $output/4_grep.txt
-rg -oPHn "smpt=........................." *.js >> $output/4_grep.txt
-rg -oPHn "token ........................." *.js >> $output/4_grep.txt
-rg -oPHn "token:........................." *.js >> $output/4_grep.txt
-rg -oPHn "token=........................." *.js >> $output/4_grep.txt
-rg -oPHn "user ........................." *.js >> $output/4_grep.txt
-rg -oPHn "user:........................." *.js >> $output/4_grep.txt
-rg -oPHn "user=........................." *.js >> $output/4_grep.txt
+
+
 rm -rf $output/3_js
 cd /root/script/3_httprobe
 sed -e '/+/d' $output/4_grep.txt | sed -e '/?/d' | sed -e '/!/d' | sed -e '/&/d' > 1.txt ; mv 1.txt $output/4_grep.txt
