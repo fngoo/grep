af7b8b1280596235bd749f864d184d80cd8b895d
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-22 05:09:48 +0800

    Update grep.sh

--- grep.sh
+++ grep.sh
@@ -19,4 +19,4 @@ rm -rf $output/3_js
 cd /root/script/3_httprobe
 sed -e '/+/d' $output/4_grep.txt | sed -e '/?/d' | sed -e '/!/d' | sed -e '/&/d' > 1.txt ; mv 1.txt $output/4_grep.txt
 touch $output/4_gp.txt ; sort -u $output/4_grep.txt -o $output/4_gp.txt ; rm $output/4_grep.txt 
-data >> /root/date.txt
+date "+%Y-%m-%d_%H:%M:%S" >> /root/date.txt ; echo 'js' >> /root/date.txt
