4c2bb72b53c21bf80416dd621809309e264626e1
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-25 13:46:32 +0800

    Update grep.sh

--- grep.sh
+++ grep.sh
@@ -13,14 +13,14 @@ rg -oPHn "admin=.................................................." *.js >> $out
 rg -oPHn "manager .................................................." *.js >> $output/4_grep.txt
 rg -oPHn "manager:.................................................." *.js >> $output/4_grep.txt
 rg -oPHn "manager=.................................................." *.js >> $output/4_grep.txt
-rg  -oPHn  "oracle" *.js >> $output/4_grep.txt
-rg  -oPHn  "mysql" *.js >> $output/4_grep.txt
-rg  -oPHn  "config" *.js >> $output/4_grep.txt
-rg  -oPHn  "url:" *.js >> $output/4_grep.txt
+rg  -oPHn  "oracle.................................................." *.js >> $output/4_grep.txt
+rg  -oPHn  "mysql...................................................." *.js >> $output/4_grep.txt
+rg  -oPHn  "config...................................................." *.js >> $output/4_grep.txt
+rg  -oPHn  "url:........................................................" *.js >> $output/4_grep.txt
 
-
-rm -rf $output/3_js
 cd /root/script/3_httprobe
+rm -rf $output/3_js
+
 sed -e '/+/d' $output/4_grep.txt | sed -e '/?/d' | sed -e '/!/d' | sed -e '/&/d' > 1.txt ; mv 1.txt $output/4_grep.txt
 touch $output/4_gp.txt ; sort -u $output/4_grep.txt -o $output/4_gp.txt ; rm $output/4_grep.txt 
 date "+%Y-%m-%d_%H:%M:%S" >> /root/date.txt ; echo 'js' >> /root/date.txt
