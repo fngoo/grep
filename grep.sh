59cff55c594992d4b6f0dde6f6cf088f18a3bc4d
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-11 18:27:43 +0800

    Update grep.sh

--- grep.sh
+++ grep.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 output=$output
 cd $output/3_js
+rg -oPHn "http.*?(?=\")" *.js >>   $output/4_grep.txt
 rg  -oPHn  "pwd .................................................." *.js >>   $output/4_grep.txt
 rg  -oPHn  "pwd:.................................................." *.js >>   $output/4_grep.txt
 rg  -oPHn  "pwd=.................................................." *.js >>   $output/4_grep.txt
@@ -20,5 +21,5 @@ cd /root/script/3_httprobe
 rm -rf $output/3_js
 
 sed -e '/+/d' $output/4_grep.txt | sed -e '/?/d' | sed -e '/!/d' | sed -e '/&/d' > 1.txt ; mv 1.txt $output/4_grep.txt
-touch $output/4_gp.txt ; sort -u $output/4_grep.txt -o $output/4_gp.txt ; rm $output/4_grep.txt 
+touch $output/4_gp.txt ; cat $output/4_grep.txt >> $output/4_gp.txt ; rm $output/4_grep.txt 
 date "+%Y-%m-%d_%H:%M:%S" >> /root/date.txt ; echo 'js' >> /root/date.txt
