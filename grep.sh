578ccd38bb3492fe410c2fef79e5456ab945d2c8
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-12 08:08:42 +0800

    Update grep.sh

--- grep.sh
+++ grep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 output=$output
 cd $output/3_js
-rg -oPHn "http.*?(?=\")" *.js >>   $output/4_grep.txt
+rg -oPHn "http.*?//.*?(?=\")" *.js >>   $output/4_grep.txt
 rg  -oPHn  "pwd .................................................." *.js >>   $output/4_grep.txt
 rg  -oPHn  "pwd:.................................................." *.js >>   $output/4_grep.txt
 rg  -oPHn  "pwd=.................................................." *.js >>   $output/4_grep.txt
